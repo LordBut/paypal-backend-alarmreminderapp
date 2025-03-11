16f78d6048e298c10e488f9792a2bb76ecc322aa
Ethan <ethanbut08@gmail.com>
2025-03-11 21:02:57 +0000

    Saving local changes before pulling

--- BackendApi.kt
+++
@@ -1,9 +0,0 @@
-package com.alarmreminderapp.backend
-
-import retrofit2.http.Body
-import retrofit2.http.POST
-
-interface BackendApi {
-  @POST("create-subscription")
-  suspend fun createSubscription(@Body request: SubscriptionRequest): SubscriptionResponse
-}
--- SubscriptionRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.alarmreminderapp.backend
-
-data class SubscriptionRequest(
-  val plan_id: String,  // REQUIRED FIELD
-  val subscriber: String = null.toString(),  // Optional field
-  val start_time: String? = null  // Optional field
-)
-
