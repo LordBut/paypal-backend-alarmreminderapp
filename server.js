09f3074c9ef2f936c48068de9ce0a68242bd125a
Ethan <ethanbut08@gmail.com>
2025-04-18 22:15:06 +0100

    whatever lalala

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ if (!process.env.FIREBASE_ADMIN_SDK_JSON) {
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 // 📌 Initialize Express
